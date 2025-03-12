e0749cac3e0f89f863d40fb3cba3a1987d85aaaa
José Ricardo <josericardo.fleury05@gmail.com>
2025-03-12 00:14:22 -0300

    repo configuration and first challenge

---
+++ README.md
@@ -0,0 +1,7 @@
+# Weekly SlothBytes Challenges
+
+Repository for the weekly tasks from the Sloth Bytes newsletter by @TheCodingSloth!
+
+His newsletter: <https://slothbytes.beehiiv.com/>
+Join here: <https://slothbytes.beehiiv.com/subscribe?ref=drtv4Fne4l>
+His YouTube channel: <https://www.youtube.com/@TheCodingSloth>
---
+++ Week1-HiddenCalculatorWords.py
@@ -0,0 +1,29 @@
+flipped_numbers = {
+    1: "I",
+    2: "Z",
+    3: "E",
+    4: "H",
+    5: "S",
+    6: "G",
+    7: "L",
+    8: "B",
+    9: "-",
+    0: "O",
+}
+
+
+def turnCalc(number) -> str:
+    """
+    This function takes a number as input and returns the word that is spelled out when the number is flipped upside down.
+    Numbers with a leading 0 need to be passed as strings due to the way Python handles leading zeros.
+    """
+    output = "".join(
+        [flipped_numbers[int(digit)] for digit in str(number) if digit.isdigit()]
+    ).upper()[::-1]
+    return output
+
+
+test_cases = [707, 5508, 3045, "07734"]
+
+for test in test_cases:
+    print(f"Input: {test} Output: {turnCalc(test)}")
